760391851c6bbb5f060cd852ae667bb63c8c8b90
yoshi-zen <yoshi180705@ruri.waseda.jp>
2024-06-11 17:23:54 +0900

    一応できたみたい

--- server/src/EightPuzzleAstar.java
+++ server/src/EightPuzzleAstar.java
@@ -1,32 +1,46 @@
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Queue;
 
 public class EightPuzzleAstar extends EightPuzzleManager {
   public void searchMethod(EightPuzzleBoard initBoard, int depth) {
     // ある深さで貯めておくキュー <盤面、スコア>保持
-    Queue<EightPuzzleBoard> depthQueue = new ArrayDeque<>();
+    Queue<EightPuzzleBoard> openList = new ArrayDeque<>();
+    // 通った経路を貯めておく
+    ArrayList<Integer[]> closedList = new ArrayList<>();
     
-    int min_heuristic = 100000;
     int f_depth = 0;
 
-    // ボードとキューの初期化
+    // ---- ボードとキューの初期化 ---
     boolean flag = false;
-    depthQueue.add(initBoard);
+    openList.add(initBoard);
 
     while (!flag) {
       // 発見フラグが立つまで繰り返し
       // まずキューに貯めてく
-      while (!depthQueue.isEmpty()) {
-        // キューから一個とる
-        EightPuzzleBoard headBoard = depthQueue.poll();
+      ArrayList<EightPuzzleBoard> depthBoards = new ArrayList<>();
 
+      // openが空なら失敗で
+      if (openList.isEmpty()) {
+        break;
+      }
+      // ある深さにおいてキューに溜まっている盤面を全て吐き出す
+      while (!openList.isEmpty()) {
+        depthBoards.add(openList.poll());
+      } 
+      // ある深さにおける先読みとキュー積み
+      for (EightPuzzleBoard headBoard: depthBoards) {
         if (ansCheck(headBoard.board)) {
           flag = true;
           System.out.println("深さ"+f_depth+"で発見！");
-          return;  // TODO: 深さを返せるようにしたい！
+          break;  // TODO: 深さを返せるようにしたい！
         }
+        
+        // ★closedリストに追加
+        closedList.add(headBoard.board);
 
+        int min_heuristic = 100000;
         int emptyIndex = Arrays.asList(headBoard.board).indexOf(0);
         boolean[] availableDirection = {emptyIndex > 2, emptyIndex % 3 != 2, emptyIndex < 6, emptyIndex % 3 != 0};
 
@@ -35,15 +49,23 @@ public class EightPuzzleAstar extends EightPuzzleManager {
         for (int i = 0; i < 4; i++) {
           if (availableDirection[i]) {
             // 各移動方向について、移動可能であれば先読みしてヒューリスティック関数計算
-            // その最小値についても同様に同様に走査
+            // その最小値についても同様に走査
             int directionDiff = direction[i];
             Integer[] swappedBoardList = swapBoardIndex(emptyIndex, directionDiff, headBoard.board);
-            heur_values[i] = heuristic2(swappedBoardList);
-            if (heur_values[i] < min_heuristic) {
-              min_heuristic = heur_values[i];
+            
+            if (!isClosedListContains(closedList, swappedBoardList)) {
+              heur_values[i] = heuristic2(swappedBoardList, f_depth+1);
+              if (heur_values[i] < min_heuristic) {
+                min_heuristic = heur_values[i];
+              }
             }
           }
         }
+
+        // for (int i: heur_values) {
+        //   System.out.print(i + " ");
+        // }
+        // System.out.println();;
 
         for (int i = 0; i < 4; i++) {
           if (availableDirection[i]) {
@@ -52,24 +74,39 @@ public class EightPuzzleAstar extends EightPuzzleManager {
             Integer[] swappedBoardList = swapBoardIndex(emptyIndex, directionDiff, headBoard.board);
             newBoard.board = Arrays.copyOf(swappedBoardList, swappedBoardList.length);
             
-            newBoard.printBoard();
-            System.out.println("-----------------");
-            if (heur_values[i] == min_heuristic) {
-              depthQueue.add(newBoard);
+            
+            // System.out.println("-----------------");
+            // System.out.println("heuristic:" + heur_values[i] + ", min_heur: " + min_heuristic);
+            // for (int bnum: newBoard.board) {
+            //   System.out.print(bnum + " ");
+            // }
+            // System.out.println();
+
+            // newBoard.printBoard();
+            
+            if (!isClosedListContains(closedList, swappedBoardList)) {
+              if (heur_values[i] == min_heuristic) {
+                openList.add(newBoard);
+                System.out.println("--------- selected --------");
+                newBoard.printBoard();
+              } else {
+                System.out.println("-----------------");
+                newBoard.printBoard();
+              }
             }
           }          
         }
       }
       f_depth++;
-    
+      System.out.println("============= depth: " + f_depth + "============");
     }
   }
 
-  static int heuristic0(Integer[] board) {
-    return 0;
+  static int heuristic0(Integer[] board, int depth) {
+    return 0 + depth;
   }
 
-  static int heuristic1(Integer[] board) {
+  static int heuristic1(Integer[] board, int depth) {
     int ans = 0;
     Integer[] correctBoard = {1,2,3,8,0,4,7,6,5};
     for (int i = 0; i < board.length; i++) {
@@ -77,10 +114,11 @@ public class EightPuzzleAstar extends EightPuzzleManager {
         ans++;
       }
     }
-    return ans;
+    // System.out.println(depth + " + " + ans);
+    return ans + depth;
   }
 
-  static int heuristic2(Integer[] board) {
+  static int heuristic2(Integer[] board, int depth) {
     int ans = 0;
     for (int i = 1; i < board.length; i++) {
       Integer[] correctBoard = {1,2,3,8,0,4,7,6,5};
@@ -88,6 +126,18 @@ public class EightPuzzleAstar extends EightPuzzleManager {
       int ansOfI = Arrays.asList(correctBoard).indexOf(i);
       ans += Math.abs(indexOfI / 3 - ansOfI / 3) + Math.abs(indexOfI % 3 - ansOfI % 3);
     }
-    return ans;
+    // System.out.println(depth + " + " + ans);
+    return ans + depth;
+  }
+
+  static boolean isClosedListContains(ArrayList<Integer[]> closedList, Integer[] list) {
+    for (Integer[] closed: closedList) {
+      boolean chker = true;
+      for (int i = 0; i < closed.length; i++) {
+        if (list[i] != closed[i]) chker = false;
+      }
+      if (chker) return true;
+    }
+    return false;
   }
 }
